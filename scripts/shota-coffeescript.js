8213154e439da1c9f5839e6ddb337e65a741cd9e
SHOTA GUTERRES <SHOTA@Peripatetica.local>
2016-05-18 13:17:48 -0700

    how bout now?, I converted the coffe to vanilla

--- scripts/shota-coffeescript.js
+++ scripts/shota-coffeescript.js
@@ -1,24 +1,28 @@
-module.exports = (robot) ->
-
-   robot.hear /badger/i, (res) ->
-     res.send "Badgers? BADGERS? WE DON'T NEED NO STINKIN BADGERS"
-  
-   robot.respond /open the (.*) doors/i, (res) ->
-     doorType = res.match[1]
-     if doorType is "pod bay"
-       res.reply "I'm afraid I can't let you do that."
-     else
-       res.reply "Opening #{doorType} doors"
-  
-   robot.hear /I like pie/i, (res) ->
-     res.emote "makes a freshly baked pie"
-  
-   lulz = ['lol', 'rofl', 'lmao']
-  
-   robot.respond /lulz/i, (res) ->
-     res.send res.random lulz
-  
-   robot.topic (res) ->
-     res.send "#{res.message.text}? That's a Paddlin'"
-  
-  
+module.exports = function(robot) {
+  var lulz;
+  robot.hear(/badger/i, function(res) {
+    return res.send("Badgers? BADGERS? WE DON'T NEED NO STINKIN BADGERS");
+  });
+  robot.respond(/open the (.*) doors/i, function(res) {
+    var doorType;
+    doorType = res.match[1];
+    if (doorType === "pod bay") {
+      return res.reply("I'm afraid I can't let you do that.");
+    } else {
+      return res.reply("Opening " + doorType + " doors");
+    }
+  });
+  robot.hear(/I like pie/i, function(res) {
+    return res.emote("makes a freshly baked pie");
+  });
+  lulz = ['lol', 'rofl', 'lmao'];
+  robot.respond(/lulz/i, function(res) {
+    return res.send(res.random(lulz));
+  });
+  return robot.topic(function(res) {
+    return res.send(res.message.text + "? That's a Paddlin'");
+  });
+  robot.hear(/These boots/, function(response) {
+    return response.send('...are made for walkin yo');
+  });
+}
